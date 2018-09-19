466eb40e20c47ca94fc6d129e7a8ea06cc455c15
tqtl911@163.com <19930911cXS>
2018-09-19 23:12:15 +0800

    Initial commit

---
+++ 1.面向过程编程.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+# Project: object-oriented 
+# Software: PyCharm
+# Time    : 2018-09-19 23:09
+# File    : 1.面向过程编程.py
+# Author  : 天晴天朗
+# Email   : tqtl@tqtl.org
+""
+"""
+
+"""
+
